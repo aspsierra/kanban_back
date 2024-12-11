2c57672fc405d8e8f3b83620445c1d05d29921f5
Álvaro Sierra <asp.sierra@gmail.com>
2024-12-11 13:06:13 +0100

    Added data to api call

--- kanban_api/serializers.py
+++ kanban_api/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from .models import Kanban, Task, Employee
 
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ['name']
+
+class TaskSerializer(serializers.ModelSerializer):
+    employees = EmployeeSerializer(many=True)
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'employees']
+
 class KanbanSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True)
     class Meta:
         model = Kanban
-        fields = ['id', 'title']
+        fields = ['id', 'title', 'tasks']
